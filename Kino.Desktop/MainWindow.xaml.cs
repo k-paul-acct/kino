6058210549d53ec37eef667301f8db0a663ea151
Pavel Kulakov <k.paul19.acc@gmail.com>
2023-10-15 21:39:09 +0300

    Desktop

---
+++ Kino.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Kino.Desktop.Models;
+using Kino.Desktop.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kino.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            mainFrame.NavigationService.Navigate(new MovieListPage());
+            //if (Context.UserNow == null)
+            //{
+            //    tbBtnExit.Visibility = Visibility.Collapsed;
+            //}
+        }
+
+        private void btnShowMovieListPage_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(new MovieListPage());
+        }
+
+        private void btnFavorites_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(new FavoritesPage());
+        }
+
+        private void btnAddMovie_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(new AddMoviePage());
+        }
+
+        private void btnToProfilePage_Click(object sender, RoutedEventArgs e)
+        {
+            if (Context.IsAuth)
+            {
+                mainFrame.NavigationService.Navigate(new ProfilePage());
+            }
+            mainFrame.NavigationService.Navigate(new AuthorizationPage());
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.NavigationService.Navigate(new MovieListPage());
+        }
+    }
+}
---
+++ Kino.Desktop/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kino.Desktop.Models
+{
+    public static class Context
+    {
+        public static bool IsAuth = false;
+
+    }
+}

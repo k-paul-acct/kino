40ff8f31909cd3a9d749388303431450664b4a85
Pavel Kulakov <k.paul19.acc@gmail.com>
2023-10-16 04:15:55 +0300

    Filtering

--- Kino.Api/Endpoints/EntityEndpoints.Title.cs
+++ Kino.Api/Endpoints/EntityEndpoints.Title.cs
@@ -1,25 +1,73 @@
-using Kino.Api.Contracts.Mapping;
 using Kino.Api.Contracts.Requests;
 using Kino.Api.Data;
+using Kino.ApiClient.Dto;
 using Kino.Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Kino.Api.Endpoints;
 
 public static partial class EntityEndpoints
 {
-    private static async Task<(Title Title, int VotesNumber)> GetTitleForPreview(int id, KinoDbContext context)
+    private static IQueryable<TitlePreviewDto> GetTitlePreviewDtos(this IQueryable<Title> titles)
     {
-        var title = await context.Titles
-            .Include(x => x.Genres)
-            .Select(x => new { Title = x, VotesNumber = x.Votes.Count, })
-            .FirstOrDefaultAsync(x => x.Title.Id == id);
-        return title is null ? default : (title.Title, title.VotesNumber);
+        return titles.Select(x => new TitlePreviewDto
+        {
+            Id = x.Id,
+            Name = x.TitleName!,
+            AdditionalName = x.TitleAdditionalName,
+            Year = x.Date!.Value,
+            ImageUrl = x.ImageUrl!,
+            VotesNumber = x.Votes.Count,
+            Genres = x.Genres.Select(y => new GenreDto
+            {
+                Id = y.Id,
+                Name = y.GenreName!,
+            }),
+        });
     }
 
-    private static Task<(Title Title, int VotesNumber)?> GetTitleForDetails(int id, KinoDbContext context)
+    private static IQueryable<TitleDetailsDto> GetTitleDetailsDtos(this IQueryable<Title> titles)
     {
-        throw new NotImplementedException();
+        return titles.Select(x => new TitleDetailsDto
+        {
+            Id = x.Id,
+            Name = x.TitleName!,
+            AdditionalName = x.TitleAdditionalName,
+            Description = x.Description,
+            Year = x.Date!.Value,
+            ImageUrl = x.ImageUrl!,
+            VotesNumber = x.Votes.Count,
+            Comments = x.Comments.Select(y => new CommentDto
+            {
+                Id = y.Id,
+                TitleId = y.TitleId!.Value,
+                Date = y.Date,
+                Text = y.TextContent!,
+                Username = y.User!.UserName!,
+                UserId = y.UserId!.Value,
+            }),
+            Genres = x.Genres.Select(y => new GenreDto
+            {
+                Id = y.Id,
+                Name = y.GenreName!,
+            }),
+            Rating = x.Votes.Average(y => y.Rating),
+        });
+    }
+
+    private static IQueryable<Title> FilterByGenres(this IQueryable<Title> titles, int[]? genreIds)
+    {
+        return genreIds.IsNullOrEmpty() ? titles : titles.Where(x => x.Genres.Any(y => genreIds!.Contains(y.Id)));
+    }
+
+    private static IQueryable<Title> FilterByQuery(this IQueryable<Title> titles, string? query)
+    {
+        return query.IsNullOrEmpty()
+            ? titles
+            : titles.Where(x => x.TitleName!.Contains(query!) ||
+                                (x.TitleAdditionalName != null &&
+                                 x.TitleAdditionalName.Contains(query!)));
     }
 
     private static void MapTitleEndpoints(this IEndpointRouteBuilder app)
@@ -46,14 +94,31 @@ public static partial class EntityEndpoints
                 context.TitleHasGenres.AddRange(genres);
                 await context.SaveChangesAsync();
 
-                var preview = await GetTitleForPreview(title.Id, context);
-                return Results.Ok(preview!.Title.MapToPreviewDto(preview.VotesNumber));
+                var preview = await context.Titles.GetTitlePreviewDtos().FirstOrDefaultAsync(x => x.Id == title.Id);
+                return Results.Ok(preview);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
                 return Results.Conflict();
             }
+        });
+
+        userApi.MapGet("", async (KinoDbContext context, int[]? genreIds, string? query) =>
+        {
+            var result = await context.Titles
+                .FilterByGenres(genreIds)
+                .FilterByQuery(query)
+                .GetTitlePreviewDtos()
+                .ToListAsync();
+
+            return Results.Ok(result);
+        });
+
+        userApi.MapGet("/{id:int}", async (int id, KinoDbContext context) =>
+        {
+            var title = await context.Titles.GetTitleDetailsDtos().FirstOrDefaultAsync(x => x.Id == id);
+            return title is null ? Results.NotFound() : Results.Ok(title);
         });
     }
 }
--- Kino.ApiClient/Dto/TitleDetailsDto.cs
+++ Kino.ApiClient/Dto/TitleDetailsDto.cs
@@ -8,7 +8,7 @@ public class TitleDetailsDto
     public string? Description { get; set; }
     public int Year { get; set; }
     public string ImageUrl { get; set; } = null!;
-    public float Rating { get; set; }
+    public double? Rating { get; set; }
     public int VotesNumber { get; set; }
     public IEnumerable<GenreDto> Genres { get; set; } = Enumerable.Empty<GenreDto>();
     public IEnumerable<CommentDto> Comments { get; set; } = Enumerable.Empty<CommentDto>();
